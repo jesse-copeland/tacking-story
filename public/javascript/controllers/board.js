1721b1ac43a2e16576b5aac049cd481379e1e362
Jesse Copeland <jesse.copeland@gmail.com>
2015-04-02 14:03:46 -1000

    flip card complete

--- public/javascript/controllers/board.js
+++ public/javascript/controllers/board.js
@@ -1,4 +1,9 @@
 angular.module('TackingStory')
 .controller('BoardController', ['$scope', 'BoardFactory', function($scope, BoardFactory){
   $scope.boards = BoardFactory.all();
+
+  $scope.flipCard = function ($event) {
+    $($event.currentTarget).closest('.flip-container').toggleClass('flip'); 
+  };
+
 }]);
